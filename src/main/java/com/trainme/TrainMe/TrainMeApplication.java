5bf2b343fcaa361c3103693368cf2edaf80b1259
kasperczykkacper@gmail.com <kasperczykkacper@gmail.com>
2023-02-20 23:48:56 +0100

    remove iks dede

--- src/main/java/com/trainme/TrainMe/TrainMeApplication.java
+++ src/main/java/com/trainme/TrainMe/TrainMeApplication.java
@@ -8,7 +8,6 @@ public class TrainMeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TrainMeApplication.class, args);
-        System.out.println("XDD");
     }
 
 }
